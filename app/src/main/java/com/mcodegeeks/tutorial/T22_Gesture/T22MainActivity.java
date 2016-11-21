cca24caf0b92988c042972ff023328542de55321
Younghwan Mun <mcodegeeks@gmail.com>
2016-11-21 09:41:20 -0500

    no message

--- app/src/main/java/com/mcodegeeks/tutorial/T22_Gesture/T22MainActivity.java
+++ app/src/main/java/com/mcodegeeks/tutorial/T22_Gesture/T22MainActivity.java
@@ -2,14 +2,43 @@ package com.mcodegeeks.tutorial.T22_Gesture;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+import android.graphics.Matrix;
 
 import com.mcodegeeks.tutorial.R;
 
 public class T22MainActivity extends Activity {
 
+    private ImageView iv;
+    private ScaleGestureDetector SGD;
+    private Matrix matrix = new Matrix();
+    private float scale = 1f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t22_main);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+        SGD = new ScaleGestureDetector(this,new ScaleListener());
     }
+
+    public boolean onTouchEvent(MotionEvent ev) {
+        SGD.onTouchEvent(ev);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+            matrix.setScale(scale, scale);
+            iv.setImageMatrix(matrix);
+            return true;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mcodegeeks/tutorial/T23_GoogleMap/MapFragment.java
@@ -0,0 +1,31 @@
+package com.mcodegeeks.tutorial.T23_GoogleMap;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.Manifest;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.mcodegeeks.tutorial.R;
+
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
