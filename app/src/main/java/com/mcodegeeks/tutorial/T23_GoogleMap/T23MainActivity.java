6c77a0b8694c65f2f6da2221fa4ae20bd7ab6e92
Younghwan Mun <mcodegeeks@gmail.com>
2016-11-21 11:02:17 -0500

    no message

--- app/src/main/java/com/mcodegeeks/tutorial/T23_GoogleMap/T23MainActivity.java
+++ app/src/main/java/com/mcodegeeks/tutorial/T23_GoogleMap/T23MainActivity.java
@@ -1,15 +1,32 @@
 package com.mcodegeeks.tutorial.T23_GoogleMap;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mcodegeeks.tutorial.R;
 
-public class T23MainActivity extends Activity {
+public class T23MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t23_main);
+
+        SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
